23664557ef6886a9ef025d828face0b2563d5ce5
himanshu-metaversity <himanshu@metaversitytechnologies.com>
2024-04-14 19:31:20 +0530

    all files initializ

--- server.js
+++ server.js
@@ -2,11 +2,42 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const cookieParser = require("cookie-parser");
-let data = "diamond";
+let data = "wolf";
+let url = "http://3.34.146.14:8080/admin-new-apis/login/is-self-by-app-url";
 
 app.use(cookieParser());
+
+const fetchApi = async (req, res) => {
+  try {
+    let payload = req?.hostname;
+    let apires = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        appUrl: payload
+      })
+    })
+    if (!apires.ok) {
+      throw new Error('response not ok');
+    }
+    let currdata = await apires.json();
+    if (currdata?.data?.ui) {
+      data = currdata?.data?.ui;
+      res.cookie("type", data, {
+        maxAge: 30000,
+        httpOnly: true,
+      });
+    }
+    console.log(currdata, 'alkdjflkajdflkajsdklf');
+  } catch (error) {
+    console.log(error, 'error')
+  }
+}
+
 // Custom middleware function to serve files conditionally
-app.use((req, res, next) => {
+app.use(async (req, res, next) => {
   const ui = req.query.ui;
   if (ui) {
     data = ui;
@@ -17,6 +48,9 @@ app.use((req, res, next) => {
     console.log(data, ui);
   } else if (req?.cookies?.type) {
     data = req?.cookies?.type;
+  } else {
+
+    await fetchApi(req, res);
   }
   // console.log(req.path);
   // app.use(express.static("diamond"));
@@ -27,14 +61,19 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(express.query());
 
-app.get("*", (req, res) => {
+app.get("*", async (req, res) => {
   const ui = req.query.ui;
   if (ui) {
     data = ui;
     res.cookie("type", ui, {
-      maxAge: 900000,
+      maxAge: 30000,
       httpOnly: true,
     });
+  } else if (req?.cookies?.type) {
+    data = req?.cookies?.type;
+  } else {
+
+    await fetchApi(req, res);
   }
 
   res.sendFile(path.join(__dirname, data, "index.html"), (err) => {
